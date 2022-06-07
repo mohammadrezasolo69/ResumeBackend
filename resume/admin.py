ac874262cfc9791425d4c81fa4b7958ba28e646a
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2022-06-07 12:03:34 +0430

    create model Skill

--- resume/admin.py
+++ resume/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from resume.models import Resume
+from resume.models import Resume, Skill
 
 
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
     list_display = ('title', 'user')
+
+
+@admin.register(Skill)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'resume')
--- resume/models.py
+++ resume/models.py
@@ -31,11 +31,26 @@ class Resume(models.Model):
     bie = models.TextField(blank=True, verbose_name='Bie')
     gender = models.CharField(max_length=1, choices=GenderChoice.choices, verbose_name='Gender')
     marital_status = models.CharField(max_length=1, choices=MaritalStatusChoice.choices, verbose_name='Marital status')
-    militaryـstatus = models.IntegerField(max_length=1, choices=MilitaryStatusChoice.choices,
+    militaryـstatus = models.IntegerField(choices=MilitaryStatusChoice.choices,
                                           verbose_name='Military status')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Skill(models.Model):
+    class LevelChoice(models.IntegerChoices):
+        EXCELLENT = 0
+        GOOD = 1
+        MEDIUM = 2
+        WEAK = 3
+
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE, related_name='skills', verbose_name='Skill')
+    title = models.CharField(max_length=100, verbose_name='Title')
+    level = models.IntegerField(choices=LevelChoice.choices, verbose_name='Level')
 
     def __str__(self):
         return self.title
