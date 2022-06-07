e9de83047b4f3450e3fa9c2ded8f0fd6be20f385
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2022-06-07 13:05:53 +0430

    inline models

--- resume/admin.py
+++ resume/admin.py
@@ -2,36 +2,43 @@ from django.contrib import admin
 from resume.models import Resume, Skill, SocialNetwork, CourseCertificate, Project, Language, Education
 
 
+# //////////////////////// Inline Models \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+class SkillInline(admin.TabularInline):
+    model = Skill
+    fields = ('title', 'level')
+
+
+class LanguageInline(admin.TabularInline):
+    model = Language
+    fields = ('language', 'level')
+
+
+class SocialNetworkInline(admin.TabularInline):
+    model = SocialNetwork
+    fields = ('title', 'link')
+
+
+class CourseCertificateInline(admin.TabularInline):
+    model = CourseCertificate
+    fields = ('title', 'link', 'institute', 'start_date', 'end_date')
+
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+    fields = ('title', 'link', 'description', 'start_date', 'end_date')
+
+
+class EducationInline(admin.TabularInline):
+    model = Education
+    fields = ('grade', 'major', 'start_date', 'end_date')
+
+
+# /////////////////////////////////////////////////////////////////////////////
+
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
     list_display = ('title', 'user')
-
-
-@admin.register(Skill)
-class SkillAdmin(admin.ModelAdmin):
-    list_display = ('title', 'level', 'resume')
-
-
-@admin.register(Language)
-class LanguageAdmin(admin.ModelAdmin):
-    list_display = ('language', 'level', 'resume')
-
-
-@admin.register(SocialNetwork)
-class SocialNetworkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'resume')
-
-
-@admin.register(CourseCertificate)
-class CourseCertificateAdmin(admin.ModelAdmin):
-    list_display = ('title', 'resume', 'institute', 'start_date', 'end_date')
-
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'resume', 'start_date', 'end_date')
-
-
-@admin.register(Education)
-class EducationAdmin(admin.ModelAdmin):
-    list_display = ('grade', 'major', 'start_date', 'end_date', 'resume')
+    inlines = [
+        SkillInline, LanguageInline, SocialNetworkInline,
+        CourseCertificateInline, ProjectInline, EducationInline
+    ]
